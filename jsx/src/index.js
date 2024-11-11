e8591b9945a37d8bb0bf97c883aa4a6a8e4cdbe4
DSkoufis <skoufisdimitris@gmail.com>
2024-11-11 22:38:51 +0000

    Use props with different types

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -13,7 +13,13 @@ function App() {
     const inputType = "number";
     const min = "5";
     return (
-        <input type={inputType} min={min} max={10}/>
+        <input
+            type={inputType}
+            min={min}
+            max={10}
+            list={[5,6,7]}
+            style={{color: 'red', width: '170px'}}
+        />
     );
 }
 
